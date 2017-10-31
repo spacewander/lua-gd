78afd1c5f1ceaed05b78ac42c297d87a493295fd
spacewander <spacewanderlzx@gmail.com>
2017-10-31 15:48:30 +0800

    Protect declaration of LgdImageCreateFromPng* with GD_PNG feature test macro
    If GD_PNG is false, neither LgdImageCreateFromPng nor
    LgdImageCreateFromPngPtr would be implemented. We should avoid declaring
    them too.

--- luagd.c
+++ luagd.c
@@ -2168,8 +2168,10 @@ static const luaL_Reg LgdFunctions[] =
     { "createFromGif",          LgdImageCreateFromGif },
     { "createFromGifStr",       LgdImageCreateFromGifPtr },
 #endif
+#ifdef GD_PNG
     { "createFromPng",          LgdImageCreateFromPng },
     { "createFromPngStr",       LgdImageCreateFromPngPtr },
+#endif
     { "createFromGd",           LgdImageCreateFromGd },
     { "createFromGdStr",        LgdImageCreateFromGdPtr },
     { "createFromGd2",          LgdImageCreateFromGd2 },
